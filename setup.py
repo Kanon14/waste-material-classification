d66f6824a73033f28d5607145cf33b3cab1c68c4
Kanon14 <geochua144@gmail.com>
2024-06-20 23:11:12 +0800

    environment setup completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "wasteDetection",
+    version = "0.0.0",
+    author = "Chua Jingxuan",
+    author_email= "geochua144@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
