3fef6f46e95f02ef07017ba48b26ee401abc8fee
Kanon14 <geochua144@gmail.com>
2024-06-22 20:31:26 +0800

    application constructed

--- app.py
+++ app.py
@@ -1,4 +1,62 @@
+import sys, os
+import cv2
 from wasteDetection.pipeline.training_pipeline import TrainPipeline
+from wasteDetection.utils.main_utils import decodeImage, encodeImageIntoBase64
+from flask import Flask, request, jsonify, render_template, Response
+from flask_cors import CORS, cross_origin
+from wasteDetection.constant.application import APP_HOST, APP_PORT
 
-obj = TrainPipeline()
-obj.run_pipeline()
+
+app = Flask(__name__)
+CORS(app)
+
+class ClientApp:
+    def __init__(self):
+        self.filename = "inputImage.jpg"
+        
+
+@app.route("/train")
+def trainRoute():
+    obj = TrainPipeline()
+    obj.run_pipeline()
+    return "Training Successfully!!"
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST", "GET"])
+@cross_origin()
+def predictRoute():
+    try:
+        image = request.json['image']
+        decodeImage(image, clApp.filename)
+        
+        os.system(f"cd yolov8n_train && yolo task=detect mode=predict \
+                    model='best.pt' \
+                    imgsz=640 \
+                    source='../data/inputImage.jpg' \
+                    conf=0.5")
+        
+        opencodedbase64 = encodeImageIntoBase64("yolov8n_train/runs/detect/predict/inputImage.jpg")
+        result = {"image": opencodedbase64.decode('utf-8')}
+        
+        os.system("rm -rf yolov8n_train/runs")
+        
+    except ValueError as val:
+        print(val)
+        return Response("Value not found inside json data")
+    except KeyError:
+        return Response("Key value error incorrect key passed")
+    except Exception as e:
+        print(e)
+        return Response("Something went wrong")
+    
+    return jsonify(result)
+        
+  
+if __name__ == "__main__":
+    clApp = ClientApp()
+    app.run(host=APP_HOST, port=APP_PORT, debug=True)
